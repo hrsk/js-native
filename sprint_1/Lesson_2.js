f1889d96a41120b59f5219a1d6bd3aba56763df3
hrsk <hrsk.yegor@gmail.com>
2025-06-15 12:12:14 +0300

    polyfill functions for array methods indexOf and reverse

--- sprint_1/Lesson_2.js
+++ sprint_1/Lesson_2.js
@@ -58,4 +58,57 @@ console.log(polyPop([]))
 console.log(numbers)
 
 
+//.indexOf -- возвращает первый индекс, по которому данный элемент может быть найден в массиве или -1, если такого индекса нет.
+
+const numbersForPolyIndexOf = [1, 2, 3, 4, 5, 6]
+
+function polyIndexOf(array, element, fromIndex = 0) {
+
+    if (fromIndex < array.length) {
+        // если fromIndex > 0 , то мы его оставляем таким как есть,
+        // а если fromIndex < 0 , то мы должны взять длину массива и отнять от него значение fromIndex
+        // и начать не с нуля, а с полученного значения индекса
+        fromIndex = fromIndex < 0 ? array.length + fromIndex : fromIndex
+
+        for (let i = fromIndex; i < array.length; i++) {
+            if (array[i] === element) {
+                return i
+            }
+        }
+    }
+    return -1
+}
+
+console.log(polyIndexOf(numbersForPolyIndexOf, 6)) // 5
+console.log(polyIndexOf(numbersForPolyIndexOf, 6, -1)) // 5
+console.log(polyIndexOf(numbersForPolyIndexOf, 10)) // -1
+console.log(polyIndexOf(numbersForPolyIndexOf, 3, 10)) // -1
+console.log(polyIndexOf(numbersForPolyIndexOf, 5, -1)) // -1
+console.log(polyIndexOf(numbersForPolyIndexOf, 5, -2)) // 4
+
+
+//.reverse() на месте обращает порядок следования элементов массива.
+// Первый элемент массива становится последним, а последний первым.
+// Возвращае ссылку на новй массив.
+
+const numbersForPolyReverse = [1, 2, 3, 4, 5, 6]
+
+function polyReverse(array) {
+
+    // делим на 2 для того что цикл остановился после выполнения, иначе он перевернет его снова и мы увидим не измененный массив
+    for (let i = 0; i < array.length / 2; i++) {
+        // создаем временную переменную
+        // записываем значение array[i] во временую переменную
+        let temp = array[i]
+        // записываем вместо 1 => 6
+        // i = 0 -- последний элемент, i = 1 -- предпоследний и т.д
+        array[i] = array[array.length - 1 - i]
+        // а вместо 6 => 1, 5 => 2, 4 => 3
+        array[array.length - 1 - i] = temp
+    }
+    return array
+}
+
+console.log(polyReverse(numbersForPolyReverse))
+
 
